7a46e72e7a18ca50e3feb5811781e319d02fce45
AniketVerma0614 <aniketverma0614@gmail.com>
2024-08-22 07:40:41 +0530

    Stopping Event Bubbling
    Stopping Event Bubbling with the help of stopPropogation() method ==> event .stopPropogation() ==> so that any child event triggered ==> does not leads to the parent event trigger ==> and the asssociated callback in the addEventListenrs() ==> function

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ div.addEventListener("click", function() {
     console.log("div was clicked");  
 });   
 
-ul.addEventListener("click", function() {  
+ul.addEventListener("click", function(event) {
+    event.stopPropagation();  
     console.log("ul was clicked");  
 });  
 
 for (let li of lis) { // Add 'let' before 'li' for proper scoping  
-    li.addEventListener("click", function() { // Fixed the typo  
+    li.addEventListener("click", function(event) { // Fixed the typo  
+        
+        event.stopPropagation();
         console.log("li was clicked !!!"); // Changed message for clarity  
     });  
 }
