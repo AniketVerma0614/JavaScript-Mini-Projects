847eced7b48196af4dbc599f8903dde57259bdd0
AniketVerma0614 <aniketverma0614@gmail.com>
2024-08-23 09:09:10 +0530

    TODO List with DOM
    Created a TODO List with Add and remove button in an unordered list ...

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-let div = document.querySelector("div");  
-let ul = document.querySelector("ul");  
-let lis = document.querySelectorAll("li"); // Changed to querySelectorAll  
+let btn= document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp =document.querySelector("input");
 
-div.addEventListener("click", function() {  
-    console.log("div was clicked");  
-});   
+btn.addEventListener("click",function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
 
-ul.addEventListener("click", function(event) {
-    event.stopPropagation();  
-    console.log("ul was clicked");  
-});  
+    let delBtn  =document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
 
-for (let li of lis) { // Add 'let' before 'li' for proper scoping  
-    li.addEventListener("click", function(event) { // Fixed the typo  
-        
-        event.stopPropagation();
-        console.log("li was clicked !!!"); // Changed message for clarity  
-    });  
-}
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value ="";
+});
+
+// let delBtns = document.querySelectorAll(".delete");
+// for (let delBtn of delBtns){
+//     delBtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
+
+ul.addEventListener("click",function(event){
+    console.log(event.target);//event.target batata hai ==> ki ye event kiske wajah se target ho ra hai !!!//  KIsne usko trigger kiya hai ...
+    if(event.target.nodeName =="BUTTON"){
+        let ListItem = event.target.parentElement;
+        ListItem.remove();
+        console.log("deleted");
+    }
+    
+});
